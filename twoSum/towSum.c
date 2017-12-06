7a40ae8fe173480b5094187d028b0a695b52d73d
special-wen <2142866269@qq.com>
2017-12-06 22:41:27 +0800

    add

--- twoSum/towSum.c
+++ twoSum/towSum.c
@@ -7,6 +7,8 @@ int main(void){
     int nums[20] = {2,7,11,15};
     int target = 9;
     int numsSize = 2;
+    int NUMS[numsSize];
+//    return NUMS;
 }
 
 int* twoSum (int* nums,int numsSize,int target){
