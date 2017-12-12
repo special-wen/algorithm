0805d23502f2826e05c90e3facd30b8255021754
special-wen <2142866269@qq.com>
2017-12-12 23:15:07 +0800

    链表排序

--- mergeTwoSortedLists/mergeTwoSortedLists.c
+++ mergeTwoSortedLists/mergeTwoSortedLists.c
@@ -3,30 +3,52 @@
 //
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct list1{
-    int num1;
-    struct list1 *next;
-}list1;
-
-typedef struct list2{
-    int num2;
-    struct list2 *next;
-}list2;
-
 typedef struct ListNode{
-    int num;
-    struct listNode *next;
-}ListNode;
+    int val;
+    struct ListNode *next;
+};
 
-list1 *head1;
-list2 *head2;
-ListNode *head;
-struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2)
+struct ListNode *l1;
+struct ListNode *l2;
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2);
 int main(void){
-    head1 = malloc(sizeof(list1));
-    head2 = malloc(sizeof(list2));
-    head1->next = NULL;
-    head2->next = NULL;
-    mergeTwoLists()
+    l1 = malloc(sizeof(struct ListNode));
+    l2 = malloc(sizeof(struct ListNode));
+    l1->next = NULL;
+    l2->next = NULL;
 }
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
+    int nums[2000];
+    struct ListNode *temp;
+    struct ListNode *flag;
+    struct ListNode *aa;
+    aa = malloc(sizeof(struct ListNode));
+    aa->next = NULL;
+    temp = l1->next;
+    flag = l2->next;
+    int i = 0;
+    int tag;
+    while (temp != NULL){
+        nums[i] =temp->val;
+        i++;
+    }
+    while (flag != NULL){
+        nums[i] = flag->val;
+        i++;
+    }
+    for(int m =0;m<i;m++){
+        for(int n = m;n<i-m;n++){
+            if(nums[m] >= nums[n]){
+                tag = nums[m];
+                nums[m] = nums[n];
+                nums[n] = tag;
+            }
+        }
+    }
+    for(int m = 0;m<i;m++){
+        aa->val = nums[m];
+        aa = aa -> next;
+    }
+    return aa;
+}
+
