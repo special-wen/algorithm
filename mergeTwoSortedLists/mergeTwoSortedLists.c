e7d8f09eb94124f61ae2c53d914b0c7d440544cd
special-wen <2142866269@qq.com>
2017-12-13 17:27:20 +0800

    链表排序

--- mergeTwoSortedLists/mergeTwoSortedLists.c
+++ mergeTwoSortedLists/mergeTwoSortedLists.c
@@ -10,45 +10,47 @@ typedef struct ListNode{
 
 struct ListNode *l1;
 struct ListNode *l2;
-struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2);
-int main(void){
-    l1 = malloc(sizeof(struct ListNode));
-    l2 = malloc(sizeof(struct ListNode));
-    l1->next = NULL;
-    l2->next = NULL;
-}
 struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
     int nums[2000];
     struct ListNode *temp;
     struct ListNode *flag;
     struct ListNode *aa;
+    
+    
     aa = malloc(sizeof(struct ListNode));
+    struct ListNode *head = aa;
     aa->next = NULL;
-    temp = l1->next;
-    flag = l2->next;
+    temp = l1;
+    flag = l2;
     int i = 0;
     int tag;
     while (temp != NULL){
         nums[i] =temp->val;
         i++;
+        temp = temp->next;
     }
     while (flag != NULL){
         nums[i] = flag->val;
         i++;
+        flag = flag->next;
     }
-    for(int m =0;m<i;m++){
-        for(int n = m;n<i-m;n++){
-            if(nums[m] >= nums[n]){
-                tag = nums[m];
-                nums[m] = nums[n];
-                nums[n] = tag;
+    int m,n;
+    for(m =0;m<i-1;m++){
+        for(n = 0;n<i-m-1;n++){
+            if(nums[n] > nums[n+1]){
+                tag = nums[n];
+                nums[n] = nums[n+1];
+                nums[n+1] = tag;
             }
         }
     }
+    struct ListNode *new;
     for(int m = 0;m<i;m++){
-        aa->val = nums[m];
+        new = malloc(sizeof(struct ListNode));
+        new->val = nums[m];
+        new->next = aa->next;
+        aa->next = new;
         aa = aa -> next;
     }
-    return aa;
+    return head->next;
 }
-
