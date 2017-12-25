b20c7df4b913ea9e270c36e52c0ba087f741e7f0
special-wen <2142866269@qq.com>
2017-12-25 21:37:35 +0800

    勇气

--- 比赛/勇气/勇气.c
+++ 比赛/勇气/勇气.c
@@ -1,30 +1,40 @@
 //
 // Created by zxw on 17-12-23.
 //
-
 #include <stdio.h>
-//int main(void){
-//}
-int NButton(int n);
-int GButton(int n);
+int max(int a);
+int min(int a);
 int main(void){
-    int n;
-    scanf("%d",&n);
-    printf("NGNG");
-    int x = 0;
-    int temp;
-    temp = NButton(x);
-
+    int value;
+    scanf("%d",&value);
+    char ch[2000];
+    int flag = 0;
+    int k = 0;
+    while (value > 0){
+        if(value %2 == 0){
+            value = max(value);
+            ch[flag] = 'G';
+            flag++;
+//            printf("G");
+        } else{
+            value = min(value);
+            ch[flag] = 'N';
+            flag++;
+//            printf("N");
+        }
+    }
+    while (flag > 0){
+        printf("%c",ch[flag-1]);
+        flag--;
+    }
 }
 
-int NButton(int n){
-    int courage;
-    courage = 2*n+1;
-    return courage;
+int max (int a){
+    int temp = (a-2)/2;
+    return temp;
 }
 
-int MButton(int n){
-    int courage;
-    courage = 2*n+2;
-    return courage;
+int min (int a){
+    int temp = (a-1)/2;
+    return temp;
 }
